cf6f474cdf758662c9e0aa6247f72033a1fbbed1
Amine <abenaceur12@gmail.com>
2019-08-12 16:35:05 -0600

    crud complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,39 @@
 class ArticlesController < ApplicationController
     def new
+        @article = Article.new() # creates articles before passing to new.html.erb
+    end
+    def edit
+        @article = Article.find(params[:id])
+    end
+ 
+    def update
+        @article = Article.find( params[:id])
+
+        if  @article.update(article_params)
+            redirect_to @article
+        else 
+            render 'edit'
+        end
     end
 
-    def index
-        @articles = Article.all
-    end
     def create
         @article = Article.new(article_params) 
         # params[:article] returns the actioncontroller paramaters object
-        @article.save
-
-        redirect_to @article
-        
+        if @article.save
+            redirect_to @article #if doesnt meet condition
+        else 
+            render 'new' #use this instead of redirect to pass @article back to the new article view
+        end
+    end
+    def index
+        @articles = Article.all
     end
     def show
         @article = Article.find(params[:id])
 
     end
+
+
 
     private
         def article_params
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+    validates :title, presence: true, 
+                    length: {minimum: 5} #validates data sent to models
 end
